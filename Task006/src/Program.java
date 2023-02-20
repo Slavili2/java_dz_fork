cb1554f6f26b28ddc4d85eb413bc6d8a54de2230
Slavchik Slavili <slavchikv@rambler.ru>
2023-02-20 10:56:00 +0700

    Реализовал функцию слияния двух отсортированных массивов не без помощи https://www.youtube.com/watch?v=wk6hUweJ4UA

--- Task006/src/Program.java
+++ Task006/src/Program.java
@@ -1,9 +1,27 @@
+import java.util.Arrays;
+
 public class Program {
     public static void main(String[] args) {
-
+        int[] uArray2 = {1,2,3,4,5,6};
+        int[] uArray1 = {1,4,7};
+        int[] result = new int[uArray1.length + uArray2.length];
+        System.out.println(Arrays.toString(result));
+        uMerge(uArray2, uArray1, result);
+        System.out.println(Arrays.toString(result));
     }
 
-    private static void uMerge(){
-        
+    private static void uMerge(int[] firstArray, int[] secondArray, int[] destArray){
+        int tempCount = firstArray.length + secondArray.length;
+        int iFirstArray = 0;
+        int iSecondArray =0;
+        for(int i = 0; i < tempCount; i++){
+            if(iFirstArray < firstArray.length && (firstArray[iFirstArray] < secondArray[iSecondArray]) || iSecondArray>=secondArray.length){
+                destArray[i] = firstArray[iFirstArray];
+                iFirstArray++;
+            } else{
+                destArray[i] =secondArray[iSecondArray];
+                iSecondArray++;
+            }
+        }
     }
 }
