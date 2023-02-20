2eafc3e3c80b2453f01e65f13674770e1288ee9c
Slavchik Slavili <slavchikv@rambler.ru>
2023-02-20 16:02:33 +0700

    rabota nad funkciey sortirovki

--- Task006/src/Program.java
+++ Task006/src/Program.java
@@ -24,4 +24,37 @@ public class Program {
             }
         }
     }
+
+    private static void uMergeForSortV1(int[] firstArray, int startFirstArray, int[] secondArray, int startSecondArray, int[] destArray, int size){
+        int tempCount = firstArray.length + secondArray.length;
+        int iFirstArray = 0;
+        int iSecondArray =0;
+        for(int i = 0; i < tempCount; i++){
+            if(iFirstArray < firstArray.length && (firstArray[iFirstArray] < secondArray[iSecondArray]) || iSecondArray>=secondArray.length){
+                destArray[i] = firstArray[iFirstArray];
+                iFirstArray++;
+            } else{
+                destArray[i] =secondArray[iSecondArray];
+                iSecondArray++;
+            }
+        }
+    }
+
+    private static int[] uMergeSortV1(int[] userArray){
+
+    int[] resultArray = userArray;
+    int stepSize = 1;//razmer massiva s konca
+
+    while(stepSize < resultArray.length){
+        for(int i = 0; i < userArray.length; i+=2*stepSize){
+
+        }
+
+        stepSize*=2;
+    }
+
+    return resultArray;
+    }
+
+
 }
